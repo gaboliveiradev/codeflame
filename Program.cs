61c36443757d7f9112a0aaa96549742b6666a2ce
gaboliveiradev <g38789138@gmail.com>
2022-10-07 10:28:45 -0300

    reconfigurações

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,17 +13,22 @@ namespace codeflame
     {
         static void Main(string[] args) // codeflame new-project name
         {
-
-
-            switch(args[0])
+            /*switch(args[0])
             {
                 case "new-project":
-                    newProject np = new newProject();
+                    string dirProject = @"C:\codeflame\projects\" + args[1];
                     string nameProject = args[1];
 
-                    np.create(nameProject);
+                    if (!Directory.Exists(dirProject))
+                    {
+                        Directory.CreateDirectory(dirProject);
+                        Console.WriteLine($"[✔] Pasta do projeto {nameProject} criada com sucesso no diretório \"{dirProject}\".");
+                    } else
+                    {
+                        Console.WriteLine("[✖] Nome de projeto existente, porfavor escolha outro.");
+                    }
                 break;
-            }
+            }*/
 
             Console.ReadKey();
         }
