019dd55db26dd5b85b7312c3a5e8df8c99109380
gaboliveiradev <g38789138@gmail.com>
2022-10-07 09:59:10 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codeflame
+{
+    internal class Program
+    {
+        static void Main(string[] args) // codeflame new-project name
+        {
+            switch(args[0])
+            {
+                case "new-project":
+                    string nameProject = args[1];
+
+                break;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
