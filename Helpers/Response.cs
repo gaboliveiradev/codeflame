781f2f3e31bafb0e928205dfeab52ceb890532d5
gaboliveiradev <g38789138@gmail.com>
2022-10-22 11:00:26 -0300

    criando a pasta response, onde contem classes error e success. Essas pastas teram as respostas das suas respectivas ações

---
+++ Helpers/Response.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codeflame.Helpers
+{
+    abstract class Response
+    {
+        public void response(string prefix, string msg)
+        {
+            Console.WriteLine($"{prefix} {msg}");
+        }
+
+    }
+}
