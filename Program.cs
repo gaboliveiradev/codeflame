664de38196a487a2c04f596d46fa3fa182354c2e
gaboliveiradev <g38789138@gmail.com>
2022-10-22 10:26:58 -0300

    reestruturando o projeto

--- Program.cs
+++ Program.cs
@@ -13,24 +13,12 @@ namespace codeflame
     {
         static void Main(string[] args)
         {
-            //codeflame create-project name --host=192.168.0.0.1 --port=3306 --user=postgres --pass=postgrespw --dbname=EtecDB
             switch (args[0])
             {
-                case "create-project":
+                case "new-project":
                     string dirRootFolder = @"C:\codeflame\projects\" + args[1] + @"\";
                     string nameProject = args[1];
 
-                    string[] infoConnection = new string[]
-                    {
-                        (args.Length > 2) ? args[2].Substring(7, args[2].Length - 7) : "localhost",
-                        (args.Length > 3) ? args[3].Substring(7, args[3].Length - 7) : "3306",
-                        (args.Length > 4) ? args[4].Substring(7, args[4].Length - 7) : "codeflame",
-                        (args.Length > 5) ? args[5].Substring(7, args[5].Length - 7) : "codeflamepw",
-                        (args.Length > 6) ? args[6].Substring(9, args[6].Length - 9) : "codeflamedb"
-                    };
-
-                    CreateProject cp = new CreateProject();
-                    cp.createRootFolder(dirRootFolder, nameProject, infoConnection);
                 break;
             }
 
