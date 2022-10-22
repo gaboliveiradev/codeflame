7bf3a12878a2e9204f069f4914486a743dd50938
gaboliveiradev <g38789138@gmail.com>
2022-10-22 14:04:46 -0300

    add de mais uma msg de error

--- Response/Error.cs
+++ Response/Error.cs
@@ -10,6 +10,7 @@ namespace codeflame.Response
     {
         public string prefix = "[X]";
 
-        public string msg_existing_project = $"Já existe um projeto com o nome \"CODEFLAME_NAME_PROJECT\", porfavor escolhar outro nome.";
+        public string msg_existing_project = "Já existe um projeto com o nome \"CODEFLAME_NAME_PROJECT\", porfavor escolhar outro nome.";
+        public string msg_create_project = "Ocorreu um erro inesperado ao tentar criar o projeto \"CODEFLAME_NAME_PROJECT\", porfavor tente novamente.";
     }
 }
