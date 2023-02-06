b525ddc2e4f2ffc30f044bd700d7be5da0511dbc
gaboliveiradev <g38789138@gmail.com>
2023-02-06 20:27:04 -0300

    Criando o instalador do codeflame + trocando a estrutura de pasta onde vai ficar os projetos

--- Program.cs
+++ Program.cs
@@ -19,7 +19,7 @@ namespace codeflame
                     NewProject newProject = new NewProject(); 
 
                     string projectName = args[1];
-                    string rootDirectory = @"C:\codeflame\projects\" + projectName + @"\App";
+                    string rootDirectory = @"C:\codeflame\repos\" + projectName + @"\App";
 
                     newProject.createRootDirectory(projectName, rootDirectory);
                 break;
