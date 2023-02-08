cac8ff6e9a29adf78aeda357b396598f5dfd677b
gaboliveiradev <g38789138@gmail.com>
2023-02-07 21:42:34 -0300

    estruturação de um novo comando para criação das camadas controller, model e dao.

---
+++ Commands/MakeLayer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codeflame.Commands
+{
+    public class MakeLayer
+    {
+
+        public void createController(string nameController)
+        {
+
+        }
+
+        public void createModel(string nameModel)
+        {
+
+        }
+
+        public void createDAO(string nameDAO)
+        {
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -13,16 +13,34 @@ namespace codeflame
     {
         static void Main(string[] args)
         {
+            NewProject newProject = new NewProject();
+            MakeLayer makeLayer = new MakeLayer();
+
             switch (args[0])
             {
                 case "new-project":
-                    NewProject newProject = new NewProject(); 
-
                     string projectName = args[1];
                     string rootDirectory = @"C:\codeflame\repos\" + projectName + @"\App";
 
                     newProject.createRootDirectory(projectName, rootDirectory);
-                break;
+                    break;
+
+                case "make:controller":
+                    string nameController = args[1];
+                    makeLayer.createController(nameController);
+                    break;
+
+
+                case "make:model":
+                    string nameModel = args[1];
+                    makeLayer.createModel(nameModel);
+                    break;
+
+
+                case "make:dao":
+                    string nameDAO = args[1];
+                    makeLayer.createDAO(nameDAO);
+                    break;
             }
 
             Console.ReadKey();
