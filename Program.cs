6aa02e2e641ac179bdfdfb666e465fbf13ef3382
gaboliveiradev <g38789138@gmail.com>
2023-03-12 09:55:37 -0300

    reajuste na sintaxe do comando

--- Program.cs
+++ Program.cs
@@ -18,28 +18,28 @@ namespace codeflame
 
             switch (args[0])
             {
-                // [Exemplo de CMD] codeflame add/project --name=EcommerceSite
-                case "add/project":
-                    string projectName = args[1].Remove(0, 7);
+                // [Exemplo de CMD] codeflame add/project EcommerceSite
+                case "add/project-mvc":
+                    string projectName = args[1];
                     string rootDirectory = @"C:\codeflame\repos\" + projectName + @"\App";
 
                     newProject.createRootDirectory(projectName, rootDirectory);
                     break;
 
-                // [Exemplo de CMD] codeflame make:controller --controller=UsuarioController
+                // [Exemplo de CMD] codeflame make:controller UsuarioController
                 case "make:controller":
-                    string nameController = args[1].Remove(0, 13);
+                    string nameController = args[1];
                     makeLayer.createController(nameController);
                     break;
 
-                // [Exemplo de CMD] codeflame make:model --model=UsuarioModel
+                // [Exemplo de CMD] codeflame make:model UsuarioModel
                 case "make:model":
                     string nameModel = args[1];
                     makeLayer.createModel(nameModel);
                     break;
 
 
-                // [Exemplo de CMD] codeflame make:dao --dao=UsuarioDAO
+                // [Exemplo de CMD] codeflame make:dao UsuarioDAO
                 case "make:dao":
                     string nameDAO = args[1];
                     makeLayer.createDAO(nameDAO);
